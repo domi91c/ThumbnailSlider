362224ad126c76811f62373d8bddce37a7baf0a8
domi91c <dominic.n@me.com>
2016-12-16 19:48:39 -0500

    simple setup for thumbnail slider

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var lBut = document.getElementById("leftButton");
+var rBut = document.getElementById("rightButton");
+
+window.addEventListener("load", function () {});
+
+lBut.onclick = function () {
+  console.log("left button clicked");
+};
+rBut.onclick = function () {
+  console.log("right button clicked");
+};
+//# sourceMappingURL=main.js.map
---
+++ main.js
@@ -0,0 +1,8 @@
+const lBut = document.getElementById("leftButton");
+const rBut = document.getElementById("rightButton");
+
+window.addEventListener("load", function () {
+});
+
+lBut.onclick = function(){console.log("left button clicked")};
+rBut.onclick = function(){console.log("right button clicked")};
