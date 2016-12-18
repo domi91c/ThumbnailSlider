8dca664f8a3204abb03136fd61d5c07105628010
domi91c <dominic.n@me.com>
2016-12-17 20:25:59 -0500

    got slide buttons working

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,86 @@
 "use strict";
 
-var lBut = document.getElementById("leftButton");
-var rBut = document.getElementById("rightButton");
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-window.addEventListener("load", function () {});
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-lBut.onclick = function () {
-  console.log("left button clicked");
-};
-rBut.onclick = function () {
-  console.log("right button clicked");
-};
+var ThumbnailSlider = function () {
+    function ThumbnailSlider() {
+        _classCallCheck(this, ThumbnailSlider);
+
+        this.slides = document.getElementsByClassName("slide");
+        this.lBut = document.getElementById("lBut");
+        this.rBut = document.getElementById("rBut");
+        this.slideIndex = 0;
+        this.slides[0].style.display = "block";
+        this.slButtons = document.getElementsByClassName("slButton");
+
+        this.previous();
+        this.next();
+        this.slideButtonClick();
+    }
+
+    _createClass(ThumbnailSlider, [{
+        key: "previous",
+        value: function previous() {
+            var _this = this;
+
+            this.lBut.onclick = function () {
+                for (var i = 0; i < _this.slides.length; i++) {
+                    _this.slides[i].style.display = "none";
+                }
+                if (_this.slideIndex < _this.slides.length - 1) {
+                    _this.slideIndex++;
+                } else {
+                    _this.slideIndex = 0;
+                }
+                _this.slides[_this.slideIndex].style.display = "block";
+            };
+        }
+    }, {
+        key: "next",
+        value: function next() {
+            var _this2 = this;
+
+            this.rBut.onclick = function () {
+                for (var i = 0; i < _this2.slides.length; i++) {
+                    _this2.slides[i].style.display = "none";
+                }
+                if (_this2.slideIndex > 0) {
+                    _this2.slideIndex--;
+                } else {
+                    _this2.slideIndex = _this2.slides.length - 1;
+                }
+                _this2.slides[_this2.slideIndex].style.display = "block";
+            };
+        }
+    }, {
+        key: "goToSlide",
+        value: function goToSlide(slideNum) {
+            for (var i = 0; i < this.slides.length; i++) {
+                this.slides[i].style.display = "none";
+            }
+            this.slides[slideNum].style.display = "block";
+        }
+    }, {
+        key: "slideButtonClick",
+        value: function slideButtonClick() {
+            var _this3 = this;
+
+            var _loop = function _loop(i) {
+                _this3.slButtons[i].onclick = function () {
+                    _this3.goToSlide(i);
+                };
+            };
+
+            for (var i = 0; i < this.slButtons.length; i++) {
+                _loop(i);
+            }
+        }
+    }]);
+
+    return ThumbnailSlider;
+}();
+
+thumbnailSlider = new ThumbnailSlider();
 //# sourceMappingURL=main.js.map
--- main.js
+++ main.js
@@ -1,8 +1,60 @@
-const lBut = document.getElementById("leftButton");
-const rBut = document.getElementById("rightButton");
+class ThumbnailSlider {
+    constructor() {
+        this.slides = document.getElementsByClassName("slide");
+        this.lBut = document.getElementById("lBut");
+        this.rBut = document.getElementById("rBut");
+        this.slideIndex = 0;
+        this.slides[0].style.display = "block";
+        this.slButtons = document.getElementsByClassName("slButton");
 
-window.addEventListener("load", function () {
-});
+        this.previous();
+        this.next();
+        this.slideButtonClick();
+    }
 
-lBut.onclick = function(){console.log("left button clicked")};
-rBut.onclick = function(){console.log("right button clicked")};
+    previous() {
+        this.lBut.onclick = () => {
+            for (let i = 0; i < this.slides.length; i++) {
+                this.slides[i].style.display = "none";
+            }
+            if (this.slideIndex < this.slides.length - 1) {
+                this.slideIndex++;
+            } else {
+                this.slideIndex = 0;
+            }
+            this.slides[this.slideIndex].style.display = "block";
+        };
+    }
+
+    next() {
+        this.rBut.onclick = () => {
+            for (let i = 0; i < this.slides.length; i++) {
+                this.slides[i].style.display = "none";
+            }
+            if (this.slideIndex > 0) {
+                this.slideIndex--;
+            } else {
+                this.slideIndex = this.slides.length - 1;
+            }
+            this.slides[this.slideIndex].style.display = "block";
+        };
+    }
+
+    goToSlide(slideNum) {
+        for (let i = 0; i < this.slides.length; i++) {
+            this.slides[i].style.display = "none";
+        }
+        this.slides[slideNum].style.display = "block";
+    }
+
+    slideButtonClick() {
+        for (let i = 0; i < this.slButtons.length; i++) {
+            this.slButtons[i].onclick = () => {
+                this.goToSlide(i)
+            }
+        }
+    }
+}
+
+
+thumbnailSlider = new ThumbnailSlider();
